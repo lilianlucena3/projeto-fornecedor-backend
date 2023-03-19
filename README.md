79f2560d96748233b27c528a291b66f017d7161c
Lilian Lucena <lilianlucena3@gmail.com>
2023-03-19 09:11:35 -0300

    projeto

---
+++ README.md
@@ -0,0 +1,10 @@
+Back-end do projeto fornecedores
+
+Primeiramente gostaria de agradecer pela oportunidade!! Esse projeto foi desenvolvido com os frameworks solicitado (Laravel e React). Os mesmos foram criado separadamente, um com o nome BackEnd e outro como frontEnd
+
+Para obter mais praticidade utilizei o Bootstrap, além do CSS.
+
+Passo a passo:
+
+Rodar o comando: git clone
+Depois instalar as dependencias: composer install
---
+++ app/Http/Controllers/ProdutoController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Produto;
+use Illuminate\Http\Request;
+
+class ProdutoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $produto = Produto::all();
+        return response()->json($produto);
+    }
+
+   
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nome' => 'required',
+            'valor' => 'required',
+            'descricao' => 'required'
+        ]);
+        $produto = Produto::create($request->all());
+        return response()->json(['message'=> 'produto criado', 
+        'produto' => $produto]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Produto  $produto
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Produto $produto)
+    {
+        return $produto;
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Produto  $produto
+     * @return \Illuminate\Http\Response
+     */
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Produto  $produto
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Produto $produto)
+    {
+        $request->validate([
+            'nome' => 'required',
+            'valor' => 'required',
+            'descricao' => 'required'
+        ]);
+       $produto->update($request->all());
+        
+        return response()->json([
+            'message' => 'produto editado!',
+            'produto' => $produto
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Produto  $produto
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Produto $produto)
+    {
+        $produto->delete();
+        return response()->json([
+            'message' => 'produto removido'
+        ]);
+        
+    }
+}
